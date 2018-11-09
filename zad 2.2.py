d13656dbad527d0c5df932f9fd1f9fc3f652256a
Bartosz Baum <bart.baum1@gmail.com>
2018-11-09 21:42:50 +0100

    Assignment 2.2 - Removed tests and added functionality to check if folder directory exist

--- zad 2.2.py
+++ zad 2.2.py
@@ -10,20 +10,18 @@ foldersList = list()
 with filedialog.askopenfile(title = 'Wybierz plik tekstowy') as f:  #asks which file to open
     foldersList = f.readlines()                                     #adds folder directory from opened file to list
 f.close() 
-print("Folder list:")                                               #tests
-print(*foldersList, sep='\n')
-print("First folder is: " + foldersList[0])
 
 ### w osobnym ustalonym folderze tworzy podfolder z nazwą wskazującą na aktualną date
 di = filedialog.askdirectory(title = 'Wybierz folder')  #asks for folder directory
 fn = di + '/' + str(datetime.now()).replace(":", "-")   #adds current date to the path as a name the for new folder
 if not os.path.exists(fn):                              #checks if path doesn't exist
     os.makedirs(fn)                                     #creates new folder in (fn) directory
-print("New folder directory is: " + fn)
 
 ### po czym kopiuje do niego wszystkie pliki z folderów z list
 for i in range(len(foldersList)):                                   #for every item (folder) in list
     print(foldersList[i])
+    if not os.path.exists(foldersList[i].strip()):                  #checks if directory from list exist
+        continue 
     files = os.listdir(foldersList[i].strip())                      #collects names of all files in specified folder, .strip() removes whitespaces from paths
     for filename in files:                                          #for every file
         fullName = os.path.join(foldersList[i].strip(), filename)   #joins file's paths with file's names to create full directory of file
